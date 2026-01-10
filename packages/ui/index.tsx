4e3c37d9951cc1f0b9c390dfbb2b8443f83964de
brianMutea <brianmuteak@gmail.com>
2026-01-10 17:42:14 +0300

    Export new canvas components from UI package

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -3,6 +3,9 @@ export * from './components/Logo';
 export * from './components/dialog';
 export * from './components/confirm-dialog';
 export * from './components/skeleton';
+export * from './components/base-canvas';
+export * from './components/view-selector';
+export * from './components/canvas-toolbar';
 
 // Contexts
 export * from './contexts/canvas-context';
@@ -10,3 +13,6 @@ export * from './contexts/canvas-context';
 // Hooks
 export * from './hooks/use-canvas-state';
 export * from './hooks/use-save-canvas';
+
+// Utils
+export * from './lib/utils';
